a8c8431ce9ef182246d57d1b7469cd306edeac65
Ajay Upadhyay <ajayu689@gmail.com>
2022-08-26 13:27:47 -0700

    change for better

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/user.js");
@@ -8,15 +9,23 @@ const mongoose = require("mongoose");
 const http = require("http");
 const cors =require("cors");
 const path=require('path')
-require("dotenv").config();
-app.use(cors());
 app.use(express.json());
+
+
+if(process.env.NODE_ENV==="development"){
+     app.use(cors());
+
+ }
+ app.use(express.static(path.join( process.cwd(), 'public')));
+
+ const {NODE_ENV, DB_USER, DB_PASSWORD, DB_HOST,DB_NAME}= process.env;
 
 const connect = async () => {
     try {
-        await mongoose.connect( `mongodb://localhost:27017/managementDB`, {
+        // await mongoose.connect(process.env.MONGO_URL)
+         await mongoose.connect( NODE_ENV === 'production'? `mongodb://${DB_HOST}/${DB_NAME}`: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority` , {
             useNewUrlParser:true,
-            useUnifiedTopology:true,
+             useUnifiedTopology:true,
         });
         console.log( 'connected to db' );
     } catch( error ) {
@@ -37,10 +46,11 @@ app.use("/users",userRouter);
 app.use("/certs",certRouter);
 
 
+
 const PORT = process.env.PORT || 4000;
 
 app.listen( PORT, () => {
-        console.log( `Server running on http://localhost:${PORT}` );
+        console.log( `Server running on port ${PORT}` );
     }) // listen() returns server
     .on( 'error', error => { // server.on( ... )
         console.error( error.message );
