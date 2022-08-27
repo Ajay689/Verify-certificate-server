182b5e3ee0b5436ad945396bb6693d8f4b4bf261
Ajay Upadhyay <ajayu689@gmail.com>
2022-08-27 14:48:17 -0700

    v1

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ if(process.env.NODE_ENV==="production"){
 }
 app.use(express.static(path.join( process.cwd(), 'public')));
 
-app.get('*',(req,res)=>{
-     res.sendFile(path.join(__dirname,'public/index.html'))
- })
+// app.get('*',(req,res)=>{
+//      res.sendFile(path.join(__dirname,'public/index.html'))
+//  })
 
  const {NODE_ENV, DB_USER, DB_PASSWORD, DB_HOST,DB_NAME}= process.env;
 
